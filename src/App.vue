d25b043d160a9a7281573c313bb62ebc2ee032d3
feylon <jamshid14092002@gmail.com>
2024-05-11 21:44:41 +0500

    salom

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="bg-gray-200 select-none flex items-center justify-center w-[100vw] h-[100vh]">
+    <div class="w-[1000px] h-[550px] bg-gray-300 flex shadow-md rounded-lg bg-white">
+      <div class="w-[50%] bg-white p-9">
+        <div class="text-blue-900 text-[20px] font-bold">Kirish</div>
+
+        <div class="text-blue-900 text-[22px] mt-[40px] font-bold">Artificial Intelligence Driving
+Results For The Travel Industry</div>
+
+
+<div class="text-gray-500 text-[11px] mt-[20px]">
+  Welcome  back! Please login to your account.
+</div>
+<div>
+  <form>
+    <div>
+
+      <label for="login">Login</label>
+    </div>
+    <div class="w-[80%]">
+      <!-- <n-input id="login" maxlength="30" show-count clearable /> -->
+      <div class="relative mb-3" data-twe-input-wrapper-init>
+  <input
+    type="text"
+    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+    placeholder="Default input" />
+  <label
+    for="exampleFormControlInpu3"
+    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
+    >Default input
+  </label>
+</div>
+    </div>
+    
+  </form>
+</div>
+
+
+      </div>
+  <div></div>
+  
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  Input,
+  initTWE,
+} from "tw-elements";
+</script>
+
+<style lang="scss" scoped>
+
+</style>
